2e3d6e148b61d74f228d75cb3e335b6080a352e5
qodjf <qianlifengxian@gmail.com>
2019-12-09 16:51:10 +0800

    [] standard function

--- 9.std.hs
+++ 9.std.hs
@@ -22,3 +22,40 @@ myReverse = g []
     g :: [a] -> [a] -> [a]
     g acc [] = acc
     g acc (a:as) = g (a:acc) as
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (l:ls) = l ++ (squish ls)
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (l:ls) = f l ++ (squishMap f ls)
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy comp (x:xs) = helper comp xs x
+  where
+    helper :: (a -> a -> Ordering) -> [a] -> a -> a
+    helper _ [] a = a
+    helper comp (x:xs) a =
+      helper comp xs (case comp a x of
+        LT -> x
+        otherwise -> a)
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy comp (x:xs) = helper comp xs x
+  where
+    helper :: (a -> a -> Ordering) -> [a] -> a -> a
+    helper _ [] a = a
+    helper comp (x:xs) a =
+      helper comp xs (case comp a x of
+        GT -> x
+        otherwise -> a)
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum = myMaximumBy compare
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum = myMinimumBy compare
