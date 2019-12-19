e2872b1d83666a0e9dd9061000f92f29ec1b5705
qodjf <qianlifengxian@gmail.com>
2019-12-19 16:29:05 +0800

    [] Product type

---
+++ chapter11/programmers.hs
@@ -0,0 +1,34 @@
+module Programmer where
+
+data OperatingSystem =
+      GnuPlusLinux
+     | OpenBSDPlusNevermindJustBSDStill
+     | Mac
+     | Windows
+     deriving (Eq, Show)
+
+data ProgrammingLanguage =
+      Haskell
+     | Agda
+     | Idris
+     | PureScript
+     deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+             , lang :: ProgrammingLanguage }
+  deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer os' language | os' <- allOperatingSystems, language <- allLanguages]
