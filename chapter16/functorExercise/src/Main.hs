ca3e73ea4ddb505e388bb314773f6c641657bcdc
qodjf <qianlifengxian@gmail.com>
2019-12-28 21:20:57 +0800

    [Functor] instance for Pair

--- chapter16/functorExercise/src/Main.hs
+++ chapter16/functorExercise/src/Main.hs
@@ -17,6 +17,7 @@ functorCompose :: (Eq (f c), Functor f) =>
 functorCompose f g x =
   (fmap g . fmap f $ x) == (fmap (g . f) x)
 
+-- Identity
 newtype Identity a = Identity a deriving (Show, Eq)
 
 instance (Arbitrary a) => Arbitrary (Identity a) where
@@ -25,7 +26,22 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
 instance Functor Identity where
   fmap f (Identity a) = Identity (f a)
 
+-- Pair
+data Pair a = Pair a a deriving (Show, Eq)
+
+instance (Arbitrary a) => Arbitrary (Pair a) where
+  arbitrary = do
+    a1 <- arbitrary
+    a2 <- arbitrary
+    return (Pair a1 a2)
+
+instance Functor Pair where
+  fmap f (Pair a1 a2) = Pair (f a1) (f a2)
+
 main :: IO ()
 main = do
   quickCheck (functorIdentity :: Identity Int -> Bool)
   quickCheck (\x -> functorCompose (+1) (*2) (x :: Identity Int))
+
+  quickCheck (functorIdentity :: Pair String -> Bool)
+  quickCheck (\x -> functorCompose (++"hello ") (++"world") (x :: Pair String))
