4d02c76a4cc1ca2d2da0d237f3307c9e27826fc6
qodjf <qianlifengxian@gmail.com>
2019-12-28 20:38:32 +0800

    [Functor] Instances of Func

---
+++ chapter16/functorExercise/src/Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Test.QuickCheck
+import Test.QuickCheck.Function
+
+functorIdentity :: (Functor f, Eq (f a)) =>
+                        f a
+                     -> Bool
+functorIdentity f =
+  fmap id f == f
+
+functorCompose :: (Eq (f c), Functor f) =>
+                       (a -> b)
+                    -> (b -> c)
+                    -> f a
+                    -> Bool
+functorCompose f g x =
+  (fmap g . fmap f $ x) == (fmap (g . f) x)
+
+newtype Identity a = Identity a deriving (Show, Eq)
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = do
+    a <- arbitrary
+    return (Identity a)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+main :: IO ()
+main = do
+  quickCheck (functorIdentity :: Identity Int -> Bool)
+  quickCheck (\x -> functorCompose (+1) (*2) (x :: Identity Int))
