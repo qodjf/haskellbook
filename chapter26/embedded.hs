9d66194ba938bc3127d452ee9768d0b90a774977
qodjf <qianlifengxian@gmail.com>
2020-01-26 18:11:09 +0800

    [monadT] embedded

---
+++ chapter26/embedded.hs
@@ -0,0 +1,8 @@
+module OuterInner where
+
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+
+embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
+embedded = MaybeT . ExceptT . ReaderT . fmap return $ (const (Right (Just 1)))
