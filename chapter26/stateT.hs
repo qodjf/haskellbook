327b79119881e1c0a3bcb9630c39f8ad637abeeb
qodjf <qianlifengxian@gmail.com>
2020-01-26 15:13:55 +0800

    [monadT] stateT

---
+++ chapter26/stateT.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE InstanceSigs #-}
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a,s) }
+
+instance (Functor m) => Functor (StateT s m) where
+  fmap :: (a -> b) -> StateT s m a -> StateT s m b
+  fmap f (StateT smas) = StateT $ \s ->
+    fmap (\(a,s1) -> (f a, s1)) (smas s)
+
+instance (Monad m) => Applicative (StateT s m) where
+  pure a = StateT $ \s -> pure (a, s)
+
+  (<*>) :: StateT s m (a -> b) -> StateT s m a -> StateT s m b
+  (StateT smfs) <*> (StateT smas) = StateT $ \s -> do
+    (f, s1) <- smfs s
+    (a, s2) <- smas s1
+    return (f a, s2)
+
+instance (Monad m) => Monad (StateT s m) where
+  return = pure
+
+  (>>=) :: StateT s m a -> (a -> StateT s m b) -> StateT s m b
+  StateT sma >>= f = StateT $ \s -> do
+    (a, s1) <- sma s
+    runStateT (f a) s1
