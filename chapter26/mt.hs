925a99312260330c8b9a0dcf4be471742d7329a3
qodjf <qianlifengxian@gmail.com>
2020-01-27 15:40:20 +0800

    [monadT] stateT, readerT

---
+++ chapter26/mt.hs
@@ -0,0 +1,19 @@
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
+import Data.Functor.Identity
+
+rDec :: Num a => Reader a a
+rDec = ReaderT . fmap Identity $ flip (-) 1
+
+rShow :: Show a => ReaderT a Identity String
+rShow = ReaderT $ Identity <$> show
+
+rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
+rPrintAndInc = ReaderT $ \a -> do
+  putStrLn $ "Hi: " ++ show a
+  return $ a + 1
+
+sPrintIncAccum :: (Num a, Show a) => StateT a IO String
+sPrintIncAccum = StateT $ \s -> do
+  putStrLn $ "Hi: " ++ show s
+  return $ (show s, s + 1) 
